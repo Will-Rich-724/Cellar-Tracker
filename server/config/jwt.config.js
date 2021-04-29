2489ae56fdbe1e226a69a2c7ba9ec011de037305
Will-Rich-724 <williamtrich5@gmail.com>
2021-04-28 19:04:44 -0500

    models and jwt config

---
+++ server/config/jwt.config.js
@@ -0,0 +1,17 @@
+const jsw = require("jsonwebtoken");
+
+module.exports = {
+    authenticate(req, res, next) {
+        jwt.verify(
+            req.cookies.usertoken,
+            process.env.JWT_SECRET,
+            (err, payload) => {
+                if (err) {
+                    res.status(401).json({verified: false})
+                } else {
+                    next();
+                }
+            }
+        )
+    }
+}
---
+++ server/models/user.model.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const BottleSchema = new mongoose.Schema({
+    wineName: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+    vintage: {
+        type: Number
+    }
+});
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        require: [true, "A first name is required"],
+        minlength: [2, "first Name must have at least two letters"]
+    },
+    lastName: {
+        type: String,
+        require: [true, "A last name is required"],
+        minlength: [2, "last Name must have at least two letters"]
+    },
+    email: {
+        type: String,
+        require: [true, "An email is required"],
+        minlength: [2, "email must have at least two letters"]
+    },
+    password: {
+        type: String,
+        require: [true, "A password is required"],
+        minlength: [2, "password must have at least two characters"]
+    },
+    bottles: {
+        type: [BottleSchema],
+        default: undefined,
+    }
+},
+    { timestamps: true }
+);
+
+UserSchema.virtual("confirmPassword")
+    .get(() => this._confirmPassword)
+    .set(value => (this._confirmPassword = value));
+
+UserSchema.pre("validate", function(next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate("confirmPassword", "Password must match confirm password")
+    }
+    next();
+});
+
+UserSchema.pre("save", function(next) {
+    bcrypt
+        .hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User
